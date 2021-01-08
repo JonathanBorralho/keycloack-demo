f5704a3a8d5b155fc0e975336940c3b3829afd10
Jonathan Borralho <jonathan.borralho@gmail.com>
2021-01-08 14:41:43 -0300

    Refatoração no cadastro de produtos

--- src/app/produtos/produtos-cadastro/produtos-cadastro.component.ts
+++ src/app/produtos/produtos-cadastro/produtos-cadastro.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { finalize } from 'rxjs/operators';
+import { ToastSeverity } from 'src/app/core/toast/toast.message';
+import { ToastService } from 'src/app/core/toast/toast.service';
+
 import { Produto } from '../models/produto.model';
 import { ProdutoService } from '../services/produto.service';
 
@@ -14,8 +18,15 @@ export class ProdutosCadastroComponent implements OnInit {
   produto: Produto;
 
   form: FormGroup;
+  loading: boolean = false;
 
-  constructor(private route: ActivatedRoute, private fb: FormBuilder, private produtoService: ProdutoService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder,
+    private produtoService: ProdutoService,
+    private toaster: ToastService,
+  ) { }
 
   ngOnInit(): void {
     this.produto = this.route.snapshot.data['produto'] as Produto;
@@ -31,21 +42,38 @@ export class ProdutosCadastroComponent implements OnInit {
     if (this.produto != null) {
       this.form.patchValue(this.produto);
     }
-
-  }
-
-  salvar() {
-    if (this.form.valid) {
-      let produto = this.form.value as Produto;
-      this.produtoService.save(produto).subscribe(
-        produto => console.log(produto),
-        err => console.log(err)
-      );
-    }
   }
 
   get editando(): boolean {
     return this.produto != null;
+  }
+
+  salvar() {
+    if (this.form.valid) {
+      this.loading = true;
+      const produto = this.form.value as Produto;
+      this.produtoService
+        .save(produto)
+        .pipe(finalize(() => this.loading = false))
+        .subscribe(this.onSuccess, this.onError);
+    }
+  }
+
+  private onSuccess = (produto: Produto) => {
+    this.toaster.show({
+      header: 'Produtos',
+      body: `${this.editando ? 'Edição realizada' : 'Cadastro realizado'} com sucesso!`,
+      severity: ToastSeverity.SUCCESS,
+    });
+    this.router.navigate(['produtos']);
+  }
+
+  private onError = (error: any) => {
+    this.toaster.show({
+      header: 'Produtos',
+      body: 'Houve um erro!',
+      severity: ToastSeverity.DANGER,
+    });
   }
 
 }
